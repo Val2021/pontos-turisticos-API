ea8028f9f0bc7deffe8ba328cfb749f7dff2b2eb
Val Araujo <val.spminas@gmail.com>
2020-09-16 10:18:00 -0300

    first commit

---
+++ core/models.py
@@ -0,0 +1,18 @@
+from django.db import models
+from atracoes.models import Atracoes
+from comentarios.models import Comentario
+from avaliacoes.models import Avaliacao
+from endereco.models import Enderecos
+
+
+class PontoTuristico(models.Model):
+    nome = models.CharField(max_length=150)
+    descricao = models.TextField()
+    aprovado = models.BooleanField(default=False)
+    atracoes = models.ManyToManyField(Atracoes)
+    comentarios = models.ManyToManyField(Comentario)
+    avaliacoes = models.ManyToManyField(Avaliacao)
+    enderecos = models.ForeignKey(Enderecos, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.nome

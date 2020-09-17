8df57a1daab7c29d0ce76c37d64d2fca6fe62f2e
Val Araujo <val.spminas@gmail.com>
2020-09-17 08:57:33 -0300

    Implementação das actions

--- core/api/viewsets.py
+++ core/api/viewsets.py
@@ -1,3 +1,5 @@
+from rest_framework.decorators import action
+from rest_framework.response import Response
 from rest_framework.viewsets import ModelViewSet
 from core.models import PontoTuristico
 from .serializers import PontoTuristicoSerializer
@@ -8,3 +10,29 @@ class PontoTuristicoViewSet(ModelViewSet):
 
     def get_queryset(self):
         return PontoTuristico.objects.filter(aprovado=True)
+
+    def list(self, request, *args, **kwargs):
+        return Response({'teste':123})
+
+    def create(self, request,*args,**kwargs):
+        return Response({'Hello': request.data['nome']})
+
+    def destroy(self, request, *args, **kwargs):
+        return
+
+    def retrieve(self, request, *args, **kwargs):
+        pass
+    def update(self, request, *args, **kwargs):
+        pass
+
+    def partial_update(self, request, *args, **kwargs):
+        pass
+
+    @action(methods=['post','get'], detail=True)
+    def denunciar(self, request, pk=None):
+        pass
+
+    @action(methods=['get'], detail=False)
+    def teste(self, request):
+        pass
+
